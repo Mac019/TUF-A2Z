d2ecfb68b50bf95bfafa6e146c3d353c7b53617b
Mac019 <makarand.jagadale916@gmail.com>
2024-11-15 13:35:02 +0530

    Right-angled traingle of stars and numbers added

--- Patterns/StarPatterns.java
+++ Patterns/StarPatterns.java
@@ -2,11 +2,30 @@ public class StarPatterns {
     public static void main(String args[]) { 
        int n = 5;
 
-       for(int i = 1; i<= 5; i++){                                // *****
-        for(int j = 1; j<=5; j++){                                // *****
-            System.out.print("*");                                // *****            
+       for(int i = 1; i<=n; i++){                                // *****
+        for(int j = 1; j<=n; j++){                                // *****
+            System.out.print("*");                              // *****            
         }                                                         // *****
         System.out.println();                                     // *****
        } 
-    } 
+    
+
+
+       for(int i = 1; i <=n; i++){                               //  *      
+        for( int j = 1; j<i; j++){                               //  **
+            System.out.print("*");                             //  ***
+        }                                                        //  ****
+        System.out.println();
+    }
+
+
+
+    for(int i = 1; i <=n; i++){                                 //  1     
+        for( int j = 1; j<=i; j++){                             //  12
+            System.out.print(j);                                //  123
+        }                                                       //  1234
+        System.out.println();                                   //  12345
+    }
+}
+
 }
