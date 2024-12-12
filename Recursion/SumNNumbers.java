8aa216493f47b4f0d3e19aebac629dd702c6ac31
Mac019 <makarand.jagadale916@gmail.com>
2024-12-12 08:26:19 +0530

    Sum of N Natural Numbers

---
+++ Recursion/SumNNumbers.java
@@ -0,0 +1,19 @@
+package Recursion;
+
+public class SumNNumbers {
+    static void func(int i, int sum) {
+        // Base Condition.
+        if (i < 1) {
+            System.out.println(sum);
+            return;
+        }
+
+        // Function call to increment sum by i till i decrements to 1.
+        func(i - 1, sum + i);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        func(n, 0);
+    }
+}
