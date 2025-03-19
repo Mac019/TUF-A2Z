06a9dc343cda4d87a9a05513f2bba64a3cc141aa
Makarand Jagadale <makarandjagadale916@gmail.com>
2025-03-19 17:34:19 +0530

    Iterative Approach O(n)

--- Recursion/Fibonacci.java
+++ Recursion/Fibonacci.java
@@ -18,3 +18,20 @@ public class Fibonacci {
         System.out.println(calculateFibonacci(number));
     }
 }
+
+
+// class Main {
+    
+    
+//     public static void main(String args[]){
+//          int first = 0;
+//         int second = 1;
+//         int n = 10;
+//         for(int i = 0; i<n; i++){
+//             int third = first + second;
+//             System.out.print(first + " " );
+//             first = second ;
+//             second = third;
+//         }
+//     }
+// }
