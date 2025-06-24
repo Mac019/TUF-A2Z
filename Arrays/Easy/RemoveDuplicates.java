7f136d5f48b2eabc8007e8d10dabb6be49973b1f
Makarand Jagadale <makarandjagadale916@gmail.com>
2025-06-24 20:54:44 +0530

    Remove Duplicatess Sorted Array

--- Arrays/Easy/RemoveDuplicates.java
+++ Arrays/Easy/RemoveDuplicates.java
@@ -1,27 +1,25 @@
 package Arrays.Easy;
 
 public class RemoveDuplicates {
-    public static void main(String[] args) {
-        int arr[] = {1, 1, 2, 2, 3, 3, 3};
-        
-        int k = removedup(arr); // Calling the function to remove duplicates
-        System.out.println("The array after removing duplicate elements is:");
-        
-        // Print only the first k elements, as they are the unique ones
-        for (int i = 0; i < k; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
 
-    static int removedup(int arr[]) {
-        int i = 0; // Pointer for the last unique element
-
-        for (int j = 1; j < arr.length; j++) { // Traverse the array starting from the second element
-            if (arr[i] != arr[j]) { // If a new unique element is found
-                i++; // Move the pointer for unique elements
-                arr[i] = arr[j]; // Update the position with the new unique element
+    static int removedups(int container[], int n) {
+        int i = 0;
+        for (int j = 1; j < n; j++) {
+            if (container[j] != container[i]) {
+                i++;
+                container[i] = container[j];
             }
         }
-        return i + 1; // The count of unique elements
+        return i + 1;
+    }
+
+    public static void main(String args[]) {
+        int container[] = {1, 1, 2, 2, 2, 3, 3};
+        int n = container.length;
+        int count = removedups(container, n);
+        for (int k = 0; k < count; k++) {
+            System.out.print(container[k] + " ");
+        }
     }
 }
+
